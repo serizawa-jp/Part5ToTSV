826a72056cfaff4c1df226b123ea1dcd4cc38ebb
SERIZAWA <serizawa@hey.com>
2021-02-20 17:46:34 +0900

    dekita

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,67 @@
 <script>
-	export let name;
+	import CopyClipBoard from "./CopyClipBoard.svelte";
+
+	let input;
+	$: output = convert(input);
+
+	const convert = (s) => {
+		if (!s) return "";
+		return parse(s)
+			.map((s) => [s.question, s.choices].flat().join("\t"))
+			.join("\n");
+	};
+
+	const parse = (s) => {
+		const arr = s
+			.split(/\r?\n/)
+			.map((s) => s.replace(/\d\s?\d\d\s?\./, ""))
+			.map((s) => s.trim())
+			.filter((s) => s !== "");
+		const problems = [];
+
+		let p = "";
+
+		for (const a of arr) {
+			p += a;
+
+			if (a.indexOf("(D)") === -1) continue;
+
+			const pos = p.indexOf("(A)");
+			problems.push({
+				question: p.substring(0, pos),
+				choices: p
+					.substring(pos)
+					.split(/\([ABCD]\)/)
+					.map((s) => s.trim())
+					.filter((s) => s !== ""),
+			});
+			p = "";
+		}
+
+		return problems;
+	};
+
+	const copy = () => {
+		const app = new CopyClipBoard({
+			target: document.getElementById("clipboard"),
+			props: { name: output },
+		});
+		app.$destroy();
+	};
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div>
+		<h1>Input</h1>
+		<textarea bind:value={input} />
+	</div>
+
+	<div>
+		<h1>Output</h1>
+		<button on:click={copy}>Copy!</button>
+		<textarea bind:value={output} disabled />
+	</div>
+	<div id="clipboard" />
 </main>
 
 <style>
@@ -20,6 +77,11 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	textarea {
+		width: 100%;
+		height: 200px;
 	}
 
 	@media (min-width: 640px) {
